40bb903eefced2def726b84bb2004855d356b915
pouletdecanon <pouletdecanon@DMVLab>
2025-07-25 12:05:05 +0200

    Automatic backup 2025-07-25_12-00

--- start.sh
+++ start.sh
@@ -2,6 +2,27 @@
 
 echo "=== Démarrage du Watchdog Minecraft ==="
 
+JAVA_FLAGS="-Xms8G \
+-Xmx8G \
+-XX:+UseG1GC \
+-XX:+ParallelRefProcEnabled \
+-XX:+UnlockExperimentalVMOptions \
+-XX:G1HeapRegionSize=8M \
+-XX:G1NewSizePercent=30 \
+-XX:G1MaxNewSizePercent=40 \
+-XX:G1ReservePercent=20 \
+-XX:InitiatingHeapOccupancyPercent=15 \
+-XX:G1MixedGCCountTarget=4 \
+-XX:G1MixedGCLiveThresholdPercent=90 \
+-XX:G1RSetUpdatingPauseTimePercent=5 \
+-XX:G1HeapWastePercent=5 \
+-XX:SurvivorRatio=32 \
+-XX:MaxTenuringThreshold=1 \
+-XX:MaxGCPauseMillis=100 \
+-XX:+DisableExplicitGC \
+-XX:+AlwaysPreTouch \
+-XX:+PerfDisableSharedMem"
+
 while true; do
   echo ">> Lancement du serveur Minecraft à $(date)"
   /start
